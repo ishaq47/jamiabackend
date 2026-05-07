9b8fddb6be51f6d00329fc98dca083413387b8e0
IshaqFrontendDev <muhammad.ishaq@simplexmed.com>
2026-05-07 17:36:31 +0500

    updated 04/05/2026

--- index.js
+++ index.js
@@ -16,12 +16,27 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://jamiafrontend-henna.vercel.app"
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("CORS blocked: " + origin));
+      }
+    },
     credentials: true
   })
-);app.use(express.json());
+);
+app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/auth', authRoutes);
